265bd8515a6ac8948c7bf856ed6dc1ce7f62b3db
Daroz <geraldodarosfilho@hotmail.com>
2022-04-06 15:43:27 -0300

    singleton

---
+++ singleton.ts
@@ -0,0 +1,14 @@
+class Unico {
+  private static instance: Unico = new Unico();
+  private constructor() {}
+
+  static getInstance(): Unico {
+        return Unico.instance;
+  }
+
+  agora(){
+      return new Date
+  }
+}
+
+console.log(Unico.getInstance().agora());
