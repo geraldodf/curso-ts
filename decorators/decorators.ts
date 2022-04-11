e915524f89d7872770ef0ff98d4a42c82b0371b8
Daroz <geraldodarosfilho@hotmail.com>
2022-04-11 17:37:08 -0300

    Criando Decorator de classe

---
+++ decorators/decorators.ts
@@ -0,0 +1,14 @@
+@logarClasse
+class Eletrodomestico {
+    constructor(){
+        console.log('Novo...');
+    }
+}
+
+function logarClasse(constructor: Function){
+    console.log(constructor);
+}
+
+new Eletrodomestico
+new Eletrodomestico
+new Eletrodomestico
