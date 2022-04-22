3fd69b81970b7d15e6d2f2a98d9f8fbb2cd5a753
Daroz <geraldodarosfilho@hotmail.com>
2022-04-22 17:20:03 -0300

    Alterando Construtor comDecorator de Classe

--- decorators/decorators.ts
+++ decorators/decorators.ts
@@ -1,14 +1,36 @@
-@logarClasse
+function logarClasse(constructor: Function) {
+  console.log(constructor);
+}
+
+function decoratorVazio(_: Function) {}
+
+function logarClasseSe(valor: boolean) {
+  return valor ? logarClasse : decoratorVazio;
+}
+
+function decorator(a: string, b: number) {
+  return function (_: Function): void {
+    console.log(a + " " + b);
+  };
+}
+//@logarClasse
+//@logarClasseSe(true)
+//@decorator("Teste", 123)
+@logarObjeto
 class Eletrodomestico {
-    constructor(){
-        console.log('Novo...');
+  constructor() {
+    console.log("Novo...");
+  }
+}
+type Construtor = { new(...args: any[]): {}}
+
+function logarObjeto (constructor: Construtor){
+    console.log('Carregado...');
+    return class extends constructor{
+        constructor(...args: any[]){
+            console.log('Antes...')
+            super(...args)
+            console.log('Depois...');
+        }
     }
 }
-
-function logarClasse(constructor: Function){
-    console.log(constructor);
-}
-
-new Eletrodomestico
-new Eletrodomestico
-new Eletrodomestico
